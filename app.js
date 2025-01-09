39621e55f8d20bf97a7543148561679d80178e04
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-09 11:19:31 +0000

    After doing some JS code

---
+++ app.js
@@ -0,0 +1,55 @@
+console.log("Hello World")
+
+let number = 1;
+console.log(number);
+
+let mints = 10;
+let chocolates = 5
+let sweets = mints + chocolates;
+
+console.log("Mints " + mints);
+console.log("Chocolates " + chocolates);
+console.log("Sweets = " + sweets);
+
+let firstName = "Liam";
+let lastName = "Collins";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+console.log(firstName + " is " + firstName.length + " characters long");
+
+let isTrue = true;
+console.log(isTrue);
+let isFalse = false;
+console.log(isFalse);
+
+let myName2 =  "Liam";
+console.log(typeof myName2);
+
+let number = 1;
+console.log("The type of number is " + typeof number);
+
+let isTrue = true;
+console.log("The type of isTrue is " + typeof isTrue);
+
+let name;
+console.log(name);
+let age = null;
+console.log(age);
+
+let a = 1;
+let b = 2;
+let isAgreaterThanB = a > b;
+let isALessThanB = a < b;
+let isAEqualToB = a === b;
+
+console.log(isAgreaterThanB);
+console.log(isALessThanB);
+console.log(isAEqualToB);
+
+
+let favouriteProgrammingLanguage = "JavaScript";
+let currentCourseDay = 3;
+
+const sentence = "My favourite programking language is " + favouriteProgrammingLanguage + " and today is day " + currentCourseDay + " of learning how to program HTML, CSS and JavaScript."
